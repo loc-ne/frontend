cbc29a9dc262eb5e42221536a062580d3bef911f
Quoc Loc Nguyen <nakrothnguyen127@gmail.com>
2024-10-15 12:05:04 +0700

    Update RegisterForm.js

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -27,7 +27,7 @@ const RegisterForm = () => {
             return;
         }
         try {
-            const response = await fetch('http://localhost:5000/register', {
+            const response = await fetch('https://backend-m0xr.onrender.com/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
